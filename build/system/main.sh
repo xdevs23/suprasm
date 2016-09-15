8354ded250bf776d36fb80859ff3bf83e584c012
Simao Gomes Viana <xdevs23@outlook.com>
2016-09-15 17:12:44 +0200

    build system: cd to src dir for relative include paths etc.

--- build/system/main.sh
+++ build/system/main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LOCALDIR="$TOP/build/system"
+ORIGDIR="$(pwd)"
 
 echo "Reading product details..."
 source $LOCALDIR/product.functions
@@ -15,7 +16,9 @@ fi
 
 echo "Starting compilation..."
 
+cd $PRODUCT_SRC_DIR
 nasm -f elf64 -o $OUT/obj/$PRODUCT_OUT_FILENAME.o $TOP/$PRODUCT_SRC_DIR/$PRODUCT_MAIN_SRC_FILE
 ld -o $OUT/bin/$PRODUCT_OUT_FILENAME $OUT/obj/$PRODUCT_OUT_FILENAME.o
+cd $ORIGDIR
 
 echo "Compilation completed."
